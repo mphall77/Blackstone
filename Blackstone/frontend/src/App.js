5d272a57093ce90268a89d8f75927cbbacee3e53
Marsha Hall <mphall77@gmail.com>
2022-03-30 18:50:43 -0400

    rough tsketch of home

--- Blackstone/frontend/src/App.js
+++ Blackstone/frontend/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// COMPONENTS
+
+//PAGES
+import Home from "./Pages/Home";
+import FourOFour from "./Pages/FourOFour";
 
 function App() {
-	return <div className="App">hello from bookings</div>;
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					{/* 404 */}
+					<Route path="*" element={<FourOFour />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- Blackstone/frontend/src/index.js
+++ Blackstone/frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
-import "./index.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./Styles/Index.scss";
 import App from "./App";
 
 const container = document.getElementById("root");
