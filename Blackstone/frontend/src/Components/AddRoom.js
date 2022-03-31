9a124e507221156cd9553834e5b730b435b8ecd0
Marsha Hall <mphall77@gmail.com>
2022-03-31 17:37:24 -0400

    add room component

--- Blackstone/frontend/src/Components/AddRoom.js
+++ Blackstone/frontend/src/Components/AddRoom.js
@@ -1,7 +1,98 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddRoom = () => {
-	return <div>AddRoom</div>;
+	const [room, setRoom] = useState({
+		name: "",
+		capacity: "",
+		floor: "",
+	});
+
+	let navigate = useNavigate();
+
+	const handleChange = (type) => {
+		return (e) => setRoom({ ...room, [type]: e.target.value });
+	};
+
+	const addNewRoom = async (newRoom) => {
+		try {
+			await axios.post(`http://localhost:3000/meeting-rooms`, newRoom);
+			navigate("/meeting-rooms");
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		addNewRoom(room);
+	};
+
+	return (
+		<section className="form_container">
+			<h2>Create a Room</h2>
+
+			<form>
+				<div className="row mb-3">
+					<label htmlFor="name" className="col-sm-2 col-form-label">
+						Room Name:
+					</label>
+					<div className="col-sm-7">
+						<input
+							type="text"
+							className="form-control"
+							id="name"
+							placeholder="Boardroom 1"
+							onChange={handleChange("name")}
+						/>
+					</div>
+				</div>
+
+				<div className="row mb-3">
+					<label htmlFor="floor" className="col-sm-2 col-form-label">
+						Floor:
+					</label>
+					<div className="col-sm-7">
+						<input
+							type="number"
+							className="form-control"
+							id="floor"
+							placeholder="22"
+							min="1"
+							max="60"
+							onChange={handleChange("floor")}
+						/>
+					</div>
+				</div>
+
+				<div className="row mb-3">
+					<label htmlFor="capacity" className="col-sm-2 col-form-label">
+						Capacity:
+					</label>
+					<div className="col-sm-7">
+						<input
+							type="number"
+							className="form-control"
+							id="capacity"
+							placeholder="0"
+							min="2"
+							max="100"
+							onChange={handleChange("capacity")}
+						/>
+					</div>
+				</div>
+
+				<button
+					type="submit"
+					className="btn btn-primary"
+					onClick={handleSubmit}
+				>
+					Submit
+				</button>
+			</form>
+		</section>
+	);
 };
 
 export default AddRoom;
