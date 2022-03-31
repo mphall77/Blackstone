54f0ab78078101f740376aed9f15f1327dd63a0b
Marsha Hall <mphall77@gmail.com>
2022-03-31 10:14:32 -0400

    add json files for rooms and bookings

--- Blackstone/frontend/src/Components/NavBar.js
+++ Blackstone/frontend/src/Components/NavBar.js
@@ -1,24 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../Styles/NavBar.scss";
+import { BiHome } from "react-icons/bi";
+// import "../Styles/NavBar.scss";
 
 const NavBar = () => {
 	return (
-		<nav className="btn-group">
-			<NavLink to={"/"} className="btn btn-primary " aria-current="page">
-				Meeting Rooms
-			</NavLink>
-			<NavLink
-				to={"/bookings"}
-				className="btn btn-primary "
-				aria-current="page"
-			>
-				Bookings
-			</NavLink>
-			<NavLink to={"/add"} className="btn btn-primary " aria-current="page">
-				New Room
-			</NavLink>
-		</nav>
+		<header>
+			<nav className="navbar">
+				<NavLink to={"/"} className="navbar__logo">
+					<BiHome style={{ fontSize: "1.8rem", color: "orangered" }} />
+				</NavLink>
+				<div className="btn-group">
+					<NavLink
+						to={"/meeting-rooms"}
+						className="btn btn-primary "
+						aria-current="page"
+					>
+						Meeting Rooms
+					</NavLink>
+					<NavLink
+						to={"/bookings"}
+						className="btn btn-primary "
+						aria-current="page"
+					>
+						Bookings
+					</NavLink>
+					<NavLink to={"/add"} className="btn btn-primary " aria-current="page">
+						New Room
+					</NavLink>
+				</div>
+			</nav>
+		</header>
 	);
 };
 
