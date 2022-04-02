c6c1280611a24c6743f3080fc4679d9157edae42
Marsha Hall <mphall77@gmail.com>
2022-04-02 11:54:56 -0400

    add EmptyList file

--- Blackstone/frontend/src/Components/RoomBookingList.js
+++ Blackstone/frontend/src/Components/RoomBookingList.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import api from "../api/database.js";
 
+// COMPONENTS
+import BookingCard from "./BookingCard.js";
+import EmptyList from "./EmptyList";
+
 const RoomBookingList = ({ room }) => {
 	const { id } = room;
 	const [bookings, setBookings] = useState([]);
@@ -19,18 +23,20 @@ const RoomBookingList = ({ room }) => {
 		fetchAllBookings();
 	}, []);
 
-	if (!id || !bookings.length) {
-		return null;
-	}
-
 	return (
 		<section>
-			List of bookings for this room
-			<div className="card">
-				{bookings.map((booking) => {
-					return <p key={booking.id}>{booking.meetingName}</p>;
-				})}
-			</div>
+			<h5>List of bookings for this room</h5>
+			{!id || (bookings.length === 0 && <EmptyList />)}
+
+			{bookings.length > 0 && (
+				<div className="card">
+					{bookings.map((booking) => {
+						return (
+							<BookingCard booking={booking} key={booking.id} room={room} />
+						);
+					})}
+				</div>
+			)}
 		</section>
 	);
 };
